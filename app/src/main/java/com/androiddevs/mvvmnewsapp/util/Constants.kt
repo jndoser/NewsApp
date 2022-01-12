136c7b5b161eed248ad375055e012efbe1068e4c
jndos <hoanglongdev533@gmail.com>
2022-01-12 17:00:06 +0700

    Initial commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "5747a91dcad24a00ab263c2e565ef1ca"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
